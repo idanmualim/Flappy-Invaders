2dec5d7da07da5b45b4835e2b4dfd75a13975480
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-11 09:48:48 -0700

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,10 +1,4 @@
 
-/**
- * Write a description of class GUI here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class GUI
 {
     // instance variables - replace the example below with your own
