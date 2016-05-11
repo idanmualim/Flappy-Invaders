9b316ca833bbd64b6b6f5b95ffe25ef8a030c375
BrandonBNguyen <BrandonBanhNguyen@gmail.com>
2016-05-11 09:50:28 -0700

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,29 +1,13 @@
-
-/**
- * Write a description of class Controller here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Controller
 {
-    // instance variables - replace the example below with your own
+    
     private GUI graphicsUserInt;
 
-    /**
-     * Constructor for objects of class Controller
-     */
     public Controller()
     {
         
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
     public void setGUI(GUI gui)
     {
         graphicsUserInt = gui;
