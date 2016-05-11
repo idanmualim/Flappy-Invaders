96b054912684883fd3ce364dc91796d50b7fbd49
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-11 10:03:14 -0700

    Update ShipType3.java

--- spaceobjects/ShipType3.java
+++ spaceobjects/ShipType3.java
@@ -1,10 +1,5 @@
+package spaceobjects;
 
-/**
- * Write a description of class EnemyShip3 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class ShipType3 extends SpaceObject
 {
     // instance variables - replace the example below with your own
