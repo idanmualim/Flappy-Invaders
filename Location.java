09ae3079d8fb070f98e5767f0472d729a75e73d6
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-11 09:52:02 -0700

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,26 @@
+package field;
+
+public class Location {
+
+	private int xPos;
+	private int yPos;
+	
+	public Location(int xPos, int yPos) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+	}
+	
+	public int getX() {
+		return xPos;
+	}
+	
+	public int getY() {
+		return yPos;
+	}
+	
+	public boolean equals(Object other) {
+		Location loc = (Location)other;
+		return (loc.getX() == xPos) && (loc.getY() == yPos);
+	}
+	
+}
