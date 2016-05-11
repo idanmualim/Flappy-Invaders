463dc2deb0f711b4d4a88c701beb46ca176b44f5
BrandonBNguyen <BrandonBanhNguyen@gmail.com>
2016-05-11 09:50:43 -0700

    Update FlappyInvaders.java

--- FlappyInvaders.java
+++ FlappyInvaders.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of class FlappyInvaders here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class FlappyInvaders
 {
     public static void main(String[] args)
