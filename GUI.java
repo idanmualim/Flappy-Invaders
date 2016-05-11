6102e653e32101952d9fb1907deef1d1a4371288
BrandonBNguyen <BrandonBanhNguyen@gmail.com>
2016-05-11 09:49:57 -0700

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,12 +1,9 @@
 
 public class GUI
 {
-    // instance variables - replace the example below with your own
+   
     private Controller control;
 
-    /**
-     * Constructor for objects of class GUI
-     */
     public GUI(Controller controller)
     {
         control = controller;
