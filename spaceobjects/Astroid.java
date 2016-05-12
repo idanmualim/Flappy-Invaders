a7a4daaf4c1faebbe4f103ed1a06b43b3570b06c
BrandonBNguyen <BrandonBanhNguyen@gmail.com>
2016-05-12 09:25:32 -0700

    Update Astroid.java

--- spaceobjects/Astroid.java
+++ spaceobjects/Astroid.java
@@ -2,12 +2,7 @@ package spaceobject;
 
 public class Astroid extends SpaceObject
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Astroids
-     */
+    
     public Astroids(Location loc, int dir, int vel)
     {
         super.(loc, dir, vel);
