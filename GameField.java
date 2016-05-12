b5583a764494008907489e80ddb643a56f3b4f81
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-12 08:38:10 -0700

    Add files via upload

---
+++ GameField.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+import spaceobjects.SpaceObject;
+
+public class GameField {
+
+	private ArrayList<SpaceObject> objects;
+	private int score;
+	private int shipsDestroyed;
+	private int timer;
+	
+	public GameField() {
+		
+	}
+	
+	public void step() {
+		
+	}
+	
+	public void checkHit() {
+		
+	}
+	
+	public SpaceObject remove(SpaceObject toRemove) {
+		return null;
+	}
+	
+}
