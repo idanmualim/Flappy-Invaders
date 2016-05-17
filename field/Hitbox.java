7530697fee3a4161cb93c17192a7213b20a9bc8e
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-17 11:04:42 -0700

    Update Hitbox.java

--- field/Hitbox.java
+++ field/Hitbox.java
@@ -3,15 +3,15 @@ package field;
 public class Hitbox
 {
     private int x;
-	private int y;
-
-	public Hitbox(Location location, int width, int height)
-	{
-		loc = location;
-		x = width;
-		y = height;
-
-	}
+    private int y;
+    
+    public Hitbox(Location location, int width, int height)
+    {
+    	loc = location;
+	x = width;
+	y = height;
+	
+    }
     
     public void changeLocation(int x, int y)
     {
@@ -23,41 +23,41 @@ public class Hitbox
     }
 
     public int getWidth() {
-		return x;
-	}
+	return x;
+    }
 
-	public int getHeight() {
-		return y;
-	}
+    public int getHeight() {
+	return y;
+    }
 
 	
-	public boolean checkCollision(Hitbox other) {
-		return (betweenX(other) && betweenY(other));
-	}
+    public boolean checkCollision(Hitbox other) {
+	return (betweenX(other) && betweenY(other));
+    }
 	
-	private boolean betweenX(Hitbox other) {
-		int xMin = loc.getX();
-		int xMax = loc.getX() + getWidth();
-		int oxMin = other.loc.getX();
-		int oxMax = other.loc.getX() + other.getWidth();
-		
-		if((xMin > oxMin && xMin < oxMax) || (xMax > oxMin && xMax < oxMax))
-			return true;
-		if((oxMin > xMin && oxMin < xMax) || (oxMax > xMin && oxMax < xMax))
-			return true;
-		return false;
-	}
+    private boolean betweenX(Hitbox other) {
+	int xMin = loc.getX();
+	int xMax = loc.getX() + getWidth();
+	int oxMin = other.loc.getX();
+	int oxMax = other.loc.getX() + other.getWidth();
 	
-	private boolean betweenY(Hitbox other) {
-		int yMin = loc.getY();
-		int yMax = loc.getY() + getHeight();
-		int oyMin = other.loc.getY();
-		int oyMax = other.loc.getY() + other.getHeight();
-		
-		if((yMin > oyMin && yMin < oyMax) || (yMax > oyMin && yMax < oyMax))
-			return true;
-		if((oyMin > yMin && oyMin < yMax) || (oyMax > yMin && oyMax < yMax))
-			return true;
-		return false;
-	}
+	if((xMin > oxMin && xMin < oxMax) || (xMax > oxMin && xMax < oxMax))
+		return true;
+	if((oxMin > xMin && oxMin < xMax) || (oxMax > xMin && oxMax < xMax))
+		return true;
+	return false;
+    }
+	
+    private boolean betweenY(Hitbox other) {
+	int yMin = loc.getY();
+	int yMax = loc.getY() + getHeight();
+	int oyMin = other.loc.getY();
+	int oyMax = other.loc.getY() + other.getHeight();
+	
+	if((yMin > oyMin && yMin < oyMax) || (yMax > oyMin && yMax < oyMax))
+		return true;
+	if((oyMin > yMin && oyMin < yMax) || (oyMax > yMin && oyMax < yMax))
+		return true;
+	return false;
+    }
 }
