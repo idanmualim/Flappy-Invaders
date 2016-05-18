bc28bc1ca8ade3da00f81e57f4df25cea251df8c
ConnorBue <connorbue@yahoo.com>
2016-05-18 10:16:01 -0700

    Update ShipType1.java

--- spaceobjects/ShipType1.java
+++ spaceobjects/ShipType1.java
@@ -2,12 +2,9 @@ package spaceobjects
 
 public class ShipType1 extends SpaceObject
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    import field.GameField;
+    import field.Location;
 
-    /**
-     * Constructor for objects of class EnemyShip
-     */
     public ShipType1(Location loc, int xVel, int yVel, GameField field, int radius) {
 		super(loc, xVel, yVel, field, radius);
     }
