9648d2d976e8d6f945bd45496b0254d0a3c30e71
ConnorBue <connorbue@yahoo.com>
2016-05-18 10:24:25 -0700

    Update ShipType1.java

--- spaceobjects/ShipType1.java
+++ spaceobjects/ShipType1.java
@@ -1,16 +1,20 @@
 package spaceobjects
 
-public class ShipType1 extends SpaceObject
-{
     import field.GameField;
     import field.Location;
-
+    
+public class ShipType1 extends SpaceObject
+{
+	private final int VELOCITY = 0; //ship stays in one place
+	
     public ShipType1(Location loc, int xVel, int yVel, GameField field, int radius) {
 		super(loc, xVel, yVel, field, radius);
     }
 
     public act()
     {
-        // moves forawrd until shot or out of screen
+        //doesnt move and shoots towards the player
+        Bullet b = new Bullet(getLocation(), false);
+		getGameField().addBulletToField(b);
     }
 }
