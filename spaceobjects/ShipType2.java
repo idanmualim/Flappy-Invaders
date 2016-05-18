3da11f1a1028c2f05fce9478d3b18b3170042aee
ConnorBue <connorbue@yahoo.com>
2016-05-18 10:40:09 -0700

    Update ShipType2.java

--- spaceobjects/ShipType2.java
+++ spaceobjects/ShipType2.java
@@ -1,21 +1,27 @@
 package spaceobjects;
 
+import field.GameField;
+import field.Location;
+
 public class ShipType2 extends SpaceObject
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
-    /**
-     * Constructor for objects of class EnemyShip2
-     */
-    public ShipType2()
-    {
-        // initialise instance variables
-        x = 0;
+    public ShipType2(Location loc, GameField field) {
+		super(loc, field);
+        
     }
 
     public act()
     {
-        // same as EnemyShip but also moves up and down
+        //moves randomly towards player does not shoot
+        //random movement generation
+        int random1 = (Math.random()*2) + 1) // up or down
+        if(random1 = 1)
+            setYVel(-(Math.random() * 3)+1) //how fast down, change if numbers are too small
+        else
+            setYVel((Math.random() * 3)+1) //how fast up
+        setXVel((Math.random() * 3)+1) //how fast forward
+        move();
+        
     }
 }
