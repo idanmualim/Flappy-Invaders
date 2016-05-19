443fffb0d4b89a7339e6c1f09bcd5730deec3008
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-19 11:05:20 -0700

    Update ShipType1.java

--- spaceobjects/ShipType1.java
+++ spaceobjects/ShipType1.java
@@ -1,23 +1,33 @@
 package spaceobjects;
 
-    import field.GameField;
-    import field.Location;
-    
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+import field.GameField;
+import field.Location;
+
 public class ShipType1 extends SpaceObject
 {
 	private final int VELOCITY = 0; //ship stays in one place
-	
-    public ShipType1(Location loc, GameField field) {
-		super(loc, field);
-    }
+	private ImageIcon img;
 
-    public act()
-    {
-        //doesnt move
-    }
-    public shoot()
-    {
-    	Bullet b = new Bullet(getLocation(), false);
-	getGameField().addBulletToField(b);
-    }
+	public ShipType1(Location loc, GameField field) {
+		super(loc, 0, 0, field);
+		img = new ImageIcon("enemy_type_1.png");
+	}
+
+	public void act()
+	{
+		//doesnt move
+	}
+	public void shoot()
+	{
+		Bullet b = new Bullet(getLocation(), false);
+		getGameField().addBulletToField(b);
+	}
+
+	public Image getImg() {
+		return img.getImage();
+	}
 }
