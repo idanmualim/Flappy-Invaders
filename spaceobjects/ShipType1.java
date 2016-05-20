e7883ab53346232f02e099290b7642e5c268461e
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-20 10:33:39 -0700

    Update ShipType1.java

--- spaceobjects/ShipType1.java
+++ spaceobjects/ShipType1.java
@@ -12,7 +12,7 @@ public class ShipType1 extends SpaceObject
 	private final int VELOCITY = 0; //ship stays in one place
 
 	public ShipType1(Location loc, GameField field) {
-		super(loc, 0, 0, field);
+		super(loc, 0, 0, field, 31, 22);
 		setImg(new ImageIcon("enemy_type_1.png").getImage());
 	}
 
