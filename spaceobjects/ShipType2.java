41ad62fac3bc67d57d062de9546108e877cf7fe6
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-20 10:34:16 -0700

    Update ShipType2.java

--- spaceobjects/ShipType2.java
+++ spaceobjects/ShipType2.java
@@ -9,7 +9,7 @@ public class ShipType2 extends SpaceObject
 {
 
     public ShipType2(Location loc, GameField field) {
-		super(loc, 0, 0, field);
+		super(loc, 0, 0, field, 27, 30);
 		setImg(new ImageIcon("enemy_type_2.png").getImage());
         
     }
