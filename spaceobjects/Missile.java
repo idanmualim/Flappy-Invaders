22caf882ce69f7f1b9c2819b5ca680f23b1c9cf2
BrandonBNguyen <BrandonBanhNguyen@gmail.com>
2016-05-20 10:37:13 -0700

    Update Missile.java

--- spaceobjects/Missile.java
+++ spaceobjects/Missile.java
@@ -2,12 +2,17 @@ package spaceobjects;
 
 public class Missile extends SpaceObject
 {
-    private final int xSIZE = //put length of missile hitbox here
-    private final int ySIZE = //put height of missile hitbox here
+    private final int WIDTH = //put width (horizontal length) of missile hitbox here
+    private final int HEIGHT = //put height (vertical length) of missile hitbox here
     private final int XSPEED = //put initial horizontal speed here (Make sure it's negative)
     
     public Missiles(Location loc, GameField field)
     {
-        super(Loc, XSPEED, 0, field, SIZE);    
+        super(loc, XSPEED, 0, field, WIDTH, HEIGHT);    
+    }
+    
+    public void changeVelocity()
+    {
+        
     }
 }
