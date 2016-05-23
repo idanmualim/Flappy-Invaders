6ca450129a67b1907f1eae9e67112fc438cc5c77
idanmualim <bobmumu19@gmail.com>
2016-05-23 09:09:20 -0700

    Update Asteroid.java

--- spaceobjects/Asteroid.java
+++ spaceobjects/Asteroid.java
@@ -2,10 +2,10 @@ package spaceobject;
 
 public class Asteroid extends SpaceObject
 {
-    private final static int WIDTH = //Place width (horizontal size) of asteroid here.
-    private final static int HEIGHT = //Place height (vertical size) of asteroid here.
-    private final static int MAXXVEL = 5 //Place maximum x velocity here
-    private final static int MAXYVEL = 5 //Place maximum y velocity here
+    private final static int WIDTH = 20;//Place width (horizontal size) of asteroid here.
+    private final static int HEIGHT = 20;//Place height (vertical size) of asteroid here.
+    private final static int MAXXVEL = 5; //Place maximum x velocity here
+    private final static int MAXYVEL = 5; //Place maximum y velocity here
     
     public Asteroids(Location loc, GameField field)
     {
