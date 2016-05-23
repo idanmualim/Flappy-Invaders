6ff33b3a43a0aeedc9aefbb6932ed5cd15a3a311
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-23 08:31:46 -0700

    Update ShipType1.java

--- spaceobjects/ShipType1.java
+++ spaceobjects/ShipType1.java
@@ -10,15 +10,22 @@ import field.Location;
 public class ShipType1 extends SpaceObject
 {
 	private final int VELOCITY = 0; //ship stays in one place
+	private int timer;
 
 	public ShipType1(Location loc, GameField field) {
 		super(loc, 0, 0, field, 31, 22);
 		setImg(new ImageIcon("enemy_type_1.png").getImage());
+		timer = 30;
 	}
 
 	public void act()
 	{
-		//doesnt move
+		if(timer == 0) {
+			shoot();
+			timer = 30;
+		}
+		else
+			timer--;
 	}
 	public void shoot()
 	{
