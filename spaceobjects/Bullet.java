40b004d6a5d5caeeab47540aff1f30fc1b5aadba
idanmualim <bobmumu19@gmail.com>
2016-05-23 09:10:17 -0700

    Update Bullet.java

--- spaceobjects/Bullet.java
+++ spaceobjects/Bullet.java
@@ -4,9 +4,9 @@ import field.Location;
 
 public class Bullet extends SpaceObject {
 
-	private final static int SPEED; //Horizontal speed of bullet
-	private final static int WIDTH; //Horizontal length of bullet
-	private final static int HEIGHT; //Vertical length of bullet
+	private final static int SPEED = 15; //Horizontal speed of bullet
+	private final static int WIDTH = 5; //Horizontal length of bullet
+	private final static int HEIGHT = 2; //Vertical length of bullet
 	
 	public Bullet(Location loc, GameField field, boolean firedByPlayer) {
 		if(firedByPlayer)
