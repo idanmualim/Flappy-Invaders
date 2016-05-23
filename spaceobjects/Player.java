826a2b83d55034ecfcacab31cefd8dd9aa5bfa99
idanmualim <bobmumu19@gmail.com>
2016-05-23 09:04:24 -0700

    Update Player.java

--- spaceobjects/Player.java
+++ spaceobjects/Player.java
@@ -8,13 +8,14 @@ public class Player extends SpaceObject{
 	private final static int INITIAL_VELOCITY = //Initial upward velocity
 	private final static int WIDTH; //Horizontal length of ship
 	private final static int HEIGHT; //Vertical length of ship
+	private final static int GRAVITY; //Set acceleration
 
 	public Player(Location loc, int xVel, int yVel, GameField field, int radius) {
 		super(loc, xVel, yVel, field, radius);
 	}
 	
 	public void act() {
-		setYVel(getYVel() - 1);
+		setYVel(getYVel() - GRAVITY);
 		move();
 	}
 	
