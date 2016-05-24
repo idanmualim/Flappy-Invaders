31a429a174b0c2f609bd9b165659f84402b44152
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-23 18:37:38 -0700

    Update Player.java

--- spaceobjects/Player.java
+++ spaceobjects/Player.java
@@ -1,26 +1,26 @@
 package spaceobjects;
 
+import javax.swing.ImageIcon;
+
 import field.GameField;
 import field.Location;
 
 public class Player extends SpaceObject{
 	
-	private final static int INITIAL_VELOCITY = 25;//Initial upward velocity
-	private final static int WIDTH = 20; //Horizontal length of ship
-	private final static int HEIGHT = 10; //Vertical length of ship
-	private final static int GRAVITY = 5; //Set acceleration
+	private final int INITIAL_VELOCITY = 5;
 
-	public Player(Location loc, int xVel, int yVel, GameField field, int radius) {
-		super(loc, xVel, yVel, field, radius);
+	public Player(Location loc, int xVel, int yVel, GameField field) {
+		super(loc, xVel, yVel, field, 0, 0);
+		setImg(new ImageIcon("src/enemy_type_1.png").getImage());
 	}
 	
 	public void act() {
-		setYVel(getYVel() - GRAVITY);
+		setYVel(getYVel() + 1);
 		move();
 	}
 	
 	public void bounce() {
-		super.setYVel(initialVelocity);
+		setYVel(INITIAL_VELOCITY);
 	}
 
 	public void shoot() {
