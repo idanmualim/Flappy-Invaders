523c91599a2d55829035738f5dfba422b7db207e
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-23 18:21:06 -0700

    Update Controller.java

--- control/Controller.java
+++ control/Controller.java
@@ -1,13 +1,55 @@
+package control;
+
+import java.util.ArrayList;
+
+import field.GameField;
+import spaceobjects.Bullet;
+import spaceobjects.SpaceObject;
+
 public class Controller {
     
     private GUI graphicsUserInt;
     private GameField field;
+    private boolean inPlay;
 
     public Controller() {
-        
+        inPlay = false;
     }
-
+    
     public void setGUI(GUI gui) {
         graphicsUserInt = gui;
+    }
+    
+    public void updateGUI() {
+    	graphicsUserInt.repaint();
+    }
+    
+    public void initField() {
+    	field = new GameField(this);
+    	inPlay = true;
+    }
+    
+    public void step() {
+    	field.step();
+    }
+    
+    public ArrayList<SpaceObject> getObjectList() {
+    	return field.getSpaceObjects();
+    }
+    
+    public ArrayList<Bullet> getBulletList() {
+    	return field.getPlayerBullets();
+    }
+    
+    public void onPressUp() {
+    	field.getPlayer().bounce();
+    }
+    
+    public void onPressSpace() {
+    	field.getPlayer().shoot();
+    }
+    
+    public boolean isInPlay() {
+    	return inPlay;
     }
 }
