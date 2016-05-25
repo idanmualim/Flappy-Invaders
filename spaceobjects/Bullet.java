97046c0c12a10e36eb863ea30f428809926185c9
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-25 09:57:44 -0700

    Update Bullet.java

--- spaceobjects/Bullet.java
+++ spaceobjects/Bullet.java
@@ -8,7 +8,7 @@ public class Bullet extends SpaceObject {
 	private final int DEFAULT_SPEED = 10;
 	
 	public Bullet(Location loc, boolean firedByPlayer) {
-		setHitbox(new Hitbox(loc, 5, 4));
+		setHitbox(new Hitbox(loc, 10, 8));
 		setLocation(loc);
 		setImg("bullet.png");
 		if(firedByPlayer)
