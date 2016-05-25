6c4273d077ad2dd5da0489db3b2a50e05d00b763
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-25 10:13:27 -0700

    Update ShipType1.java

--- spaceobjects/ShipType1.java
+++ spaceobjects/ShipType1.java
@@ -5,24 +5,31 @@ import field.Location;
 public class ShipType1 extends SpaceObject
 {
 	private final static int VELOCITY = 0; //ship stays in one place
-	private final static int FIRERATE = 30;
+	private final static int FIRERATE = 60;
 	private int timer;
 
 	public ShipType1(Location loc, GameField field) {
-		super(loc, 0, 0, field, 62, 44);
+		super(loc, -15, 0, field, 62, 44);
 		setImg("ship1.png");
 		timer = FIRERATE;
 	}
 
 	public void act()
 	{
-		if(timer <= 0) {
+		move();
+		if(getXVel() < 0) {
+			setXVel(getXVel() + 0.7);
+			timer = FIRERATE;
+		}
+		
+		timer--;
+		if (timer <= 0)
+		{
 			shoot();
 			timer = FIRERATE;
 		}
-		else
-			timer--;
-		move();
+			
+		
 	}
 
 }
