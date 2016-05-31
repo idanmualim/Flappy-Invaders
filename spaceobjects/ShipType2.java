003c132f8637edb3a238c0ecf1d17be0b8b10c6a
robertfurth <r.m.f.signupagree@gmail.com>
2016-05-31 08:28:17 -0700

    Update ShipType2.java

--- spaceobjects/ShipType2.java
+++ spaceobjects/ShipType2.java
@@ -5,39 +5,23 @@ import field.Location;
 
 public class ShipType2 extends SpaceObject
 {
-	private final static int MOVERATE = 30;
 	int timer;
-	
-    public ShipType2(Location loc, GameField field) {
-		super(loc, 0, 0, field, 54, 60);
-		setImg("ship2.png");
-		timer = MOVERATE;
-        
-    }
 
-    public void act()
-    {
-        if(timer <= 0) {
-		changeVelocity();
-		timer = MOVERATE;
+	public ShipType2(Location loc, GameField field) {
+		super(loc, -10, 0, field, 54, 60);
+		setImg("ship2.png");
+		timer = 0;
+
 	}
-	else
-		timer--;
-	
-	if(getLocation().getY() >= 536 || getLocation().getY() <= 64)
-		setYVel(-getYVel());
-        move();
-        
-    }
-    
-    public void changeVelocity() {
-    	//moves randomly towards player does not shoot
-        //random movement generation
-        int random1 = (int) ((Math.random()*2) + 1); // up or down
-        if(random1 == 1)
-            setYVel(-(Math.random() * 5 + 5)); //how fast down, change if numbers are too small
-        else
-            setYVel(Math.random() * 5 + 5); //how fast up
-        setXVel(-(Math.random() * 5 + 5)); //how fast forward
-    }
+
+	public void act()
+	{
+		timer++;
+		changeVelocity();
+		move();
+	}
+
+	public void changeVelocity() {
+		setYVel(-Math.cos(timer * Math.PI / 20) * 10);
+	}
 }
