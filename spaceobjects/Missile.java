8dbd5bdfcd8e59a133c3296b640baf3e2e7aed71
robertfurth <r.m.f.signupagree@gmail.com>
2016-06-01 10:26:28 -0700

    Update Missile.java

--- spaceobjects/Missile.java
+++ spaceobjects/Missile.java
@@ -5,25 +5,22 @@ import field.Location;
 
 public class Missile extends SpaceObject
 {
-    private static final int WIDTH = 20;//put width (horizontal length) of missile hitbox here
-    private static final int HEIGHT = 5;//put height (vertical length) of missile hitbox here
-    private static final int INITXSPEED = -5;//put initial horizontal speed here (Make sure it's negative)
-    private static final int ACCELERATION = -3;//put acceleration of missile to the left. (Make sure it's negative)
+    private static final int WIDTH = 50;//put width (horizontal length) of missile hitbox here
+    private static final int HEIGHT = 32;//put height (vertical length) of missile hitbox here
+    private static final int INITXSPEED = 0;//put initial horizontal speed here (Make sure it's negative)
+    private static final double ACCELERATION = -0.5;//put acceleration of missile to the left. (Make sure it's negative)
     private final static int SCORE = 100;
-    private int xSpeed;
     
     public Missile(Location loc, GameField field)
     {
     	super(loc, INITXSPEED, 0, field, WIDTH, HEIGHT);
-    	xSpeed = INITXSPEED;
     	setImg("missile.png");
     }
     
     public void act()
     {
-        xSpeed += ACCELERATON;
-        setXVel(xSpeed + ACCELERATION);
-        move;
+        setXVel(getXVel() + ACCELERATION);
+        move();
     }
     
     public int getScore() {
